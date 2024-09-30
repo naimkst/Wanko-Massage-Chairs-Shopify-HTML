22218f504073cb24f197e987ceffaabbb69047a2
shahin-shohan <56084608+shahin-shohan@users.noreply.github.com>
2024-09-30 22:14:02 +0600

    add 2 page

--- assets/script.js
+++ assets/script.js
@@ -350,6 +350,63 @@
 
     productGallary();
 
+
+    /*------------------------------------------
+        = CONTACT FORM SUBMISSION
+    -------------------------------------------*/
+    if ($("#contact-form-main").length) {
+        $("#contact-form-main").validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+
+                email: "required",
+
+                phone: "required",
+
+                adress: "required",
+
+                service: "required",   
+
+            },
+
+            messages: {
+                name: "Please enter your name",
+                email: "Please enter your email address",
+                phone: "Please enter your phone number",
+                adress: "Please enter your adress",
+                service: "Please select your contact service",
+            },
+
+            submitHandler: function (form) {
+                $.ajax({
+                    type: "POST",
+                    url: "mail-contact.php",
+                    data: $(form).serialize(),
+                    success: function () {
+                        $( "#loader").hide();
+                        $( "#success").slideDown( "slow" );
+                        setTimeout(function() {
+                        $( "#success").slideUp( "slow" );
+                        }, 3000);
+                        form.reset();
+                    },
+                    error: function() {
+                        $( "#loader").hide();
+                        $( "#error").slideDown( "slow" );
+                        setTimeout(function() {
+                        $( "#error").slideUp( "slow" );
+                        }, 3000);
+                    }
+                });
+                return false; // required to block normal submit since you used ajax
+            }
+
+        });
+    }
+
     /*------------------------------------------
         = BACK TO TOP BTN SETTING
     -------------------------------------------*/
